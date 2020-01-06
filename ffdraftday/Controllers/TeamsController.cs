1fed40ae27a84c96e2d9311473d454f3843a1c19
Vince Dionisio <vdionisio@avifoodsystems.com>
2020-01-05 20:38:02 -0500

    Edit draft positions

---
+++ ffdraftday/Controllers/TeamsController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ffdraftday.Models;
+using Microsoft.AspNetCore.Mvc;
+using ffdraftday.Repos;
+
+namespace ffdraftday.Controllers
+{
+    public class TeamsController : Controller
+    {
+        private ffdraftdayContext _db;
+        private Repo _repo;
+        public TeamsController(ffdraftdayContext db, Repo repo)
+        {
+            _db = db;
+            _repo = repo;
+        }
+        
+        public IActionResult Details(int id)
+        {
+            if (id == 0) return NotFound();
+            var team = _repo.teams.Get(id);
+            if (team == null) return NotFound();
+            return View(team);
+        }
+
+        public IActionResult Edit(int id)
+        {
+            if (id == 0) return NotFound();
+            var team = _repo.teams.Get(id);
+            if (team == null) return NotFound();
+            return View(team);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Team changes)
+        {
+            if (ModelState.IsValid)
+            {
+                _repo.teams.Update(changes);
+                return RedirectToAction("Details", new { id = changes.Id });
+            }
+            return View(changes);
+        }
+
+        public IActionResult Move(int draftId)
+        {
+            var teams = _repo.teams.List(draftId);
+            ViewBag.Draft = _repo.drafts.Get(draftId);
+            return View(teams);
+        }
+
+        [HttpPost]
+        public IActionResult Move(int id, string direction)
+        {
+            if (id == 0) return NotFound("No team id passed");
+            var team = _repo.teams.Get(id);
+            if (team == null) return NotFound("invalid team");
+            _repo.teams.Move(id, (direction == "Move Up" ? -1 : 1));
+            return RedirectToAction("Move", new { draftId = team.DraftId });
+        }
+    }
+}
--- ffdraftday/Repos/TeamRepo.cs
+++ ffdraftday/Repos/TeamRepo.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using ffdraftday.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace ffdraftday.Repos
 {
@@ -16,7 +17,9 @@ namespace ffdraftday.Repos
 
         public Team Get(int id)
         {
-            var team = _db.Team.Where(t => t.Id == id)
+            var team = _db.Team
+                .Include(t => t.Draft)
+                .Where(t => t.Id == id)
                 .FirstOrDefault();
             return team;
         }
@@ -31,6 +34,40 @@ namespace ffdraftday.Repos
         {
             var pos = _db.Team.Count(t => t.DraftId == draftId) + 1;
             _db.Team.Add(new Team { DraftId = draftId, DraftPosition = pos, Name = $"Team {pos}" });
+            _db.SaveChanges();
+        }
+
+        public void Update(Team changes)
+        {
+            var team = _db.Team.Find(changes.Id);
+            if (team == null) return;
+            team.Name = changes.Name;
+            team.Owner = changes.Owner;
+            if (changes.DraftPosition > 0 && changes.DraftPosition <= 20) team.DraftPosition = changes.DraftPosition;
+            _db.Update(team);
+            _db.SaveChanges();
+        }
+
+        public List<Team> List(int draftId)
+        {
+            var teams = _db.Team.OrderBy(t => t.DraftPosition).ToList();
+            return teams;
+        }
+
+        internal void Move(int id, int direction)
+        {
+            var movingTeam  = Get(id);
+            if (movingTeam == null) throw new Exception("Team not found");
+            var maxPos = _db.Team.Count();
+            var oldPos = _db.Team.Count(t => t.DraftPosition <= movingTeam.DraftPosition);
+            var newPos = direction == -1 ? (oldPos > 1 ? oldPos - 1 : 1) : (oldPos < maxPos ? oldPos + 1 : maxPos);
+            var pos = 0;
+            foreach(Team team in _db.Team.OrderBy(t => t.DraftPosition))
+            {
+                pos += 1;
+                team.DraftPosition = pos == oldPos ? newPos : (pos == newPos ? oldPos : pos);
+                _db.Update(team);   
+            }
             _db.SaveChanges();
         }
     }
